bbfda7a71094a3818325cdf150e5dea96e3da662
TaishiU <nbadreamteam3@gmail.com>
2021-08-07 13:05:46 +0900

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-void main() {
+/*バックグラウンド用*/
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('バックグラウンドでメッセージを受け取りました！');
+}
+
+/*フォアグラウンド用*/
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+  'high_importance_channel', // id
+  'High Importance Notifications', // title
+  'This channel is used for important notifications.', // description
+);
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  /*バックグラウンド用*/
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(MyApp());
 }
 
@@ -26,6 +49,43 @@ class PushNotification extends StatefulWidget {
 }
 
 class _PushNotificationState extends State<PushNotification> {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+
+  _getToken() {
+    _firebaseMessaging.getToken().then((deviceToken) {
+      print('device token: $deviceToken');
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getToken();
+
+    /*フォアグラウンド用*/
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('フォアグラウンドでメッセージを受け取りました！');
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification!.android;
+
+      if (notification != null && android != null) {
+        flutterLocalNotificationsPlugin.show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              channel.id,
+              channel.name,
+              channel.description,
+              icon: 'launch_background',
+            ),
+          ),
+        );
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
